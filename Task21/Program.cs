ce242f3d444e57d083c1e1e87f1957fb035ac184
Funko Sergey <Funko@List.ru>
2022-06-20 11:53:22 +0300

    ADD HomeWork3

---
+++ Task21/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+*/
+
+
+
+using System;
+using  static System.Console;
+
+Clear();
+WriteLine("Данная програма выводит расстояние между двух введенных координат в 3D пространстве ");
+Write ("Введите координаты первой точки (A) по осям Х: ");
+int X1 = int.Parse(ReadLine());
+Write (" Y: ");
+int Y1 = int.Parse(ReadLine());
+Write (" Z: ");
+int Z1 = int.Parse(ReadLine());
+WriteLine();
+Write ("Введите координаты второй точки (B) по осям Х: ");
+int X2 = int.Parse(ReadLine());
+Write (" Y: ");
+int Y2 = int.Parse(ReadLine());
+Write (" Z: ");
+int Z2 = int.Parse(ReadLine());
+
+WriteLine ($"Расстояние между точками A ({X1},{Y1},{Z1}) и B ({X2},{Y2},{Z2}) следующее: {Math.Sqrt(Math.Pow(X2 - X1,2)+Math.Pow(Y2 - Y1,2)+Math.Pow(Z2 - Z1,2)):f2}");
---
+++ Task23/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+using System;
+using  static System.Console;
+
+Clear();
+WriteLine("Данная програма выводит последовательность кубов чисел от 1 до введенного числа N");
+Write("Просьба ввести число: ");
+int N = int.Parse(ReadLine());
+WriteLine ($"Последовательность из кубов от 1 до {N} следующая:");
+if (N > 0)
+{
+    for (int i = 1; i < N; i++)
+    {
+        Write ($"{Math.Pow(i,3)}, ");
+    }
+    WriteLine ($"{Math.Pow(N,3)}");
+}
+else
+{
+    for (int i = 1; i > N; i--)
+    {
+        Write ($"{Math.Pow(i,3)}, ");
+    }
+    WriteLine ($"{Math.Pow(N,3)}");
+}
